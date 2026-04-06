f18643f9ce30df5ec7fd792efcd4ed1ec5a94fad
a22tonmarmar <a22tonmarmar@inspedralbes.cat>
2026-04-06 20:21:20 +0200

    feat: addició d'endpoint de login i funcionalitat

--- backend/main-back/routes/api.php
+++ backend/main-back/routes/api.php
@@ -1,7 +1,11 @@
 <?php
+
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EventController;
-use App\Http\Controllers\TiquetController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\TiquetController;
+
+Route::post('login', [AuthController::class, 'login']);
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('tiquets', TiquetController::class);
