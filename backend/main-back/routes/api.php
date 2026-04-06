3b60c8b20f0894c405df0f677923d03471b2271d
a22tonmarmar <a22tonmarmar@inspedralbes.cat>
2026-04-06 20:23:17 +0200

    feat: protecció admin de sanctum (possiblement canvii en un futur)

--- backend/main-back/routes/api.php
+++ backend/main-back/routes/api.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\TiquetController;
 
 Route::post('login', [AuthController::class, 'login']);
 
-Route::apiResource('events', EventController::class);
-Route::apiResource('tiquets', TiquetController::class);
-Route::apiResource('orders', OrderController::class)->only(['store']);
-Route::get('orders/event/{event}', [OrderController::class, 'ordersByEvent']);
+Route::middleware('auth:sanctum')->group(function () {
+	Route::apiResource('events', EventController::class);
+	Route::apiResource('tiquets', TiquetController::class);
+	Route::apiResource('orders', OrderController::class)->only(['store']);
+	Route::get('orders/event/{event}', [OrderController::class, 'ordersByEvent']);
+});
